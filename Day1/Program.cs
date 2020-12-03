0e111e24f559858922cef6e2c9ef05eefe62d3d5
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2020-12-03 08:43:26 +0000

    complete day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = DataInput.Get();
+            var calculation = new Dictionary<int, int>();
+            foreach (var number in data)
+            {
+                var result = 2020 - number;
+                if (!calculation.ContainsKey(result))
+                    calculation.Add(result, number);
+            }
+
+            foreach (var number in data)
+            {
+                if (calculation.ContainsKey(number))
+                {
+                    Console.WriteLine($"{number}, {calculation[number]}");
+                    Console.WriteLine($"{number * calculation[number]}");
+                    break;
+                }
+            }
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = i + 1; j < data.Length; j++)
+                {
+                    if (calculation.ContainsKey(data[i]+data[j]))
+                    {
+                        Console.WriteLine($"{data[i]}, {data[j]}, {calculation[data[i]+data[j]]}");
+                        Console.WriteLine($"{data[i] * data[j] * calculation[data[i] + data[j]]}");
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
