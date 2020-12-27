10d369bc9d33ab54b0ce55ed98a0b6ea520fd0e5
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2020-12-27 10:40:58 +0000

    Day 9 part 1

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Day9
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var file = File.ReadAllLines("input.txt");
+            int preambleSize = 25;
+
+            var explorer = new long[preambleSize];
+
+            for (int i = 0; i < preambleSize; i++)
+                explorer[i] = long.Parse(file[i]);
+
+            for (var index = preambleSize; index < file.Length; index++)
+            {
+                var row = file[index];
+                var belongs = CheckIfBelongs(row, explorer);
+                if (!belongs)
+                {
+                    Console.WriteLine($"{row} does not belong!");
+                    break;
+                }
+
+                PushNewValue(explorer, row);
+            }
+        }
+
+        private static void PushNewValue(long[] explorer, string row)
+        {
+            for (int i = 1; i < explorer.Length; i++)
+            {
+                explorer[i - 1] = explorer[i];
+            }
+
+            explorer[^1] = long.Parse(row);
+        }
+
+        public static bool CheckIfBelongs(string row, long[] queue)
+        {
+            //if A = B + C -> A-B = C
+            long value = long.Parse(row);
+            Dictionary<long, bool> operation = new Dictionary<long, bool>();
+            foreach (var l in queue)
+            {
+                if (!operation.ContainsKey(l))
+                    operation.Add(l, true);
+            }
+
+            foreach (var l in queue)
+            {
+                if (operation.ContainsKey(value - l))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
