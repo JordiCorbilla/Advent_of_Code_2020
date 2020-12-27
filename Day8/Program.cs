de96abc5c70772d3bcc029a0bfa46137dd386b63
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2020-12-27 09:32:14 +0000

    finish day 8

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -85,6 +86,82 @@ namespace Day8
             }
 
             Console.WriteLine(Accumulator);
+
+            Part2();
+        }
+
+        public static void Part2()
+        {
+            var file = File.ReadAllLines("inputfix.txt");
+            var listInstructions = file.Select(row => row.Split(" "))
+                .Select(ins => new Instruction(ins[0], ins[1])).ToList();
+            var listInstructionsToTest = file.Select(row => row.Split(" "))
+                .Select(ins => new Instruction(ins[0], ins[1])).ToList();
+
+            for (int i = 0; i < listInstructionsToTest.Count; i++)
+            {
+                if (listInstructionsToTest[i].Operation == InstructionOperation.jmp)
+                {
+                    listInstructions[i].Operation = InstructionOperation.nop;
+                    Accumulator = 0;
+                    var success = PerformBootCheck(listInstructions);
+                    if (success)
+                    {
+                        Console.WriteLine($"success instruction {i} {listInstructions[i].Operation} {listInstructions[i].Value}");
+                        break;
+                    }
+
+                    listInstructions = file.Select(row => row.Split(" "))
+                        .Select(ins => new Instruction(ins[0], ins[1])).ToList();
+                }
+                if (listInstructionsToTest[i].Operation == InstructionOperation.nop)
+                {
+                    listInstructions[i].Operation = InstructionOperation.jmp;
+                    Accumulator = 0;
+                    var success = PerformBootCheck(listInstructions);
+                    if (success)
+                    {
+                        Console.WriteLine($"success instruction {i} {listInstructions[i].Operation} {listInstructions[i].Value}");
+                        break;
+                    }
+
+                    listInstructions = file.Select(row => row.Split(" "))
+                        .Select(ins => new Instruction(ins[0], ins[1])).ToList();
+                }
+            }
+            Console.WriteLine(Accumulator);
+        }
+
+        public static bool PerformBootCheck(List<Instruction> listInstructions)
+        {
+            var index = 0;
+            var maxInstructions = listInstructions.Count;
+            while (index != maxInstructions)
+            {
+                var op = listInstructions[index];
+                if (op.Visited)
+                {
+                    break;
+                }
+
+                switch (op.Operation)
+                {
+                    case InstructionOperation.acc:
+                        Accumulator += op.Value;
+                        index++;
+                        break;
+                    case InstructionOperation.jmp:
+                        index += op.Value;
+                        break;
+                    case InstructionOperation.nop:
+                        index++;
+                        break;
+                }
+
+                op.Visited = true;
+            }
+
+            return index == maxInstructions;
         }
     }
 }
