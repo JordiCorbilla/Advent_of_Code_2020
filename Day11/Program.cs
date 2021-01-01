cedd8285811faee7cbcd20f33ea73938c69b95f9
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2021-01-01 16:15:12 +0000

    Update Program.cs

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -12,14 +12,20 @@ namespace Day11
 
         private static void Main()
         {
+            Part1();
+            Part2();
+        }
+
+        private static void Part1()
+        {
             var seating = File.ReadAllLines("input.txt").ToList();
 
             var seat = -1;
             var row = 0;
             var backup = Clone(seating);
 
-            MaxSeats = seating[0].Length-1;
-            MaxRows = seating.Count-1;
+            MaxSeats = seating[0].Length - 1;
+            MaxRows = seating.Count - 1;
 
             var previous1 = Clone(seating);
             while (SeatKeepsChanging(backup, previous1) || seat == -1)
@@ -39,6 +45,7 @@ namespace Day11
                             row++;
                         }
                     }
+
                     var changed = CheckMostAdjacent(seat, row, previous1);
                     merged = Merge(previous, changed, seat, row);
                     previous = Clone(merged);
@@ -48,6 +55,51 @@ namespace Day11
                 row = 0;
                 seat = 0;
             }
+
+            var occupied = CountOccupiedSeats(backup);
+            Console.WriteLine(occupied);
+        }
+
+        private static void Part2()
+        {
+            var seating = File.ReadAllLines("input.txt").ToList();
+
+            var seat = -1;
+            var row = 0;
+            var backup = Clone(seating);
+
+            MaxSeats = seating[0].Length - 1;
+            MaxRows = seating.Count - 1;
+
+            var previous1 = Clone(seating);
+            while (SeatKeepsChanging(backup, previous1) || seat == -1)
+            {
+                List<string> merged = null;
+                previous1 = Clone(backup);
+                var previous = Clone(previous1);
+                while (seat < MaxSeats || row < MaxRows)
+                {
+                    if (seat < MaxSeats)
+                        seat++;
+                    else
+                    {
+                        seat = 0;
+                        if (row < MaxRows)
+                        {
+                            row++;
+                        }
+                    }
+
+                    var changed = CheckDeepAdjacent(seat, row, previous1);
+                    merged = Merge(previous, changed, seat, row);
+                    previous = Clone(merged);
+                }
+
+                backup = Clone(merged);
+                row = 0;
+                seat = 0;
+            }
+
             var occupied = CountOccupiedSeats(backup);
             Console.WriteLine(occupied);
         }
@@ -246,6 +298,185 @@ namespace Day11
                     backup[row] = newArrangement;
                     break;
                 }
+            }
+
+            return backup;
+        }
+
+        private static List<string> CheckDeepAdjacent(int seat, int row, List<string> seatingPlan)
+        {
+            var backup = Clone(seatingPlan);
+            //Check the 8 adjacent sits
+            var currentSeat = backup[row][seat];
+            if (currentSeat == '.') return backup;
+            var empty = 0;
+            var max = 0;
+            var occupied = 0;
+
+            // XXX
+            // XXO
+            // XXX
+            if (seat + 1 <= MaxSeats)
+            {
+                max++;
+                var next = backup[row][seat + 1];
+                switch (next)
+                {
+                    case 'L':
+                    case '.':
+                        empty++;
+                        break;
+                    case '#':
+                        occupied++;
+                        break;
+                }
+            }
+            // XXX
+            // XXX
+            // XXO
+            if (seat + 1 <= MaxSeats && row + 1 <= MaxRows)
+            {
+                max++;
+                var next = backup[row + 1][seat + 1];
+                switch (next)
+                {
+                    case 'L':
+                    case '.':
+                        empty++;
+                        break;
+                    case '#':
+                        occupied++;
+                        break;
+                }
+            }
+            // XXX
+            // XXX
+            // XOX
+            if (row + 1 <= MaxRows)
+            {
+                max++;
+                var next = backup[row + 1][seat];
+                switch (next)
+                {
+                    case 'L':
+                    case '.':
+                        empty++;
+                        break;
+                    case '#':
+                        occupied++;
+                        break;
+                }
+            }
+            // XXX
+            // XXX
+            // OXX
+            if (seat - 1 >= 0 && row + 1 <= MaxRows)
+            {
+                max++;
+                var next = backup[row + 1][seat - 1];
+                switch (next)
+                {
+                    case 'L':
+                    case '.':
+                        empty++;
+                        break;
+                    case '#':
+                        occupied++;
+                        break;
+                }
+            }
+            // XXX
+            // OXX
+            // XXX
+            if (seat - 1 >= 0)
+            {
+                max++;
+                var next = backup[row][seat - 1];
+                switch (next)
+                {
+                    case 'L':
+                    case '.':
+                        empty++;
+                        break;
+                    case '#':
+                        occupied++;
+                        break;
+                }
+            }
+            // OXX
+            // XXX
+            // XXX
+            if (seat - 1 >= 0 && row - 1 >= 0)
+            {
+                max++;
+                var next = backup[row - 1][seat - 1];
+                switch (next)
+                {
+                    case 'L':
+                    case '.':
+                        empty++;
+                        break;
+                    case '#':
+                        occupied++;
+                        break;
+                }
+            }
+            // XOX
+            // XXX
+            // XXX
+            if (row - 1 >= 0)
+            {
+                max++;
+                var next = backup[row - 1][seat];
+                switch (next)
+                {
+                    case 'L':
+                    case '.':
+                        empty++;
+                        break;
+                    case '#':
+                        occupied++;
+                        break;
+                }
+            }
+            // XXO
+            // XXX
+            // XXX
+            if (seat + 1 <= MaxSeats && row - 1 >= 0)
+            {
+                max++;
+                var next = backup[row - 1][seat + 1];
+                switch (next)
+                {
+                    case 'L':
+                    case '.':
+                        empty++;
+                        break;
+                    case '#':
+                        occupied++;
+                        break;
+                }
+            }
+            switch (currentSeat)
+            {
+                case 'L' when max == empty:
+                    {
+                        var seats = backup[row];
+                        var chars = seats.ToCharArray();
+                        chars[seat] = '#';
+                        var newArrangement = new string(chars);
+                        backup[row] = newArrangement;
+                        break;
+                    }
+                case '#' when occupied >= 4:
+                    {
+                        var seats = backup[row];
+                        var chars = seats.ToCharArray();
+                        chars[seat] = 'L';
+                        var newArrangement = new string(chars);
+                        backup[row] = newArrangement;
+                        break;
+                    }
             }
 
             return backup;
