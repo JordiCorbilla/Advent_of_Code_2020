3849f416f486901efb05e28bb31f40166b90900a
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2021-02-18 21:53:10 +0000

    scaffolding day 20

--- Day20/Program.cs
+++ Day20/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Day20
 {
+    public class Tile
+    {
+        public int Id { get; set; }
+        public List<string> Content { get; set; }
+
+        public Tile()
+        {
+            Content = new List<string>();
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var file = File.ReadAllLines("input.txt");
+            var tiles = new List<Tile>();
+            bool start = false;
+            int id = -1;
+            Tile tile = null;
+            foreach (var row in file)
+            {
+                if (start && !string.IsNullOrEmpty(row))
+                {
+                    tile.Content.Add(row);
+                }
+                else if (string.IsNullOrEmpty(row))
+                {
+                    start = false;
+                    tiles.Add(tile);
+                }
+                if (row.Contains("Tile"))
+                {
+                    tile = new Tile {Id = int.Parse(row.Replace("Tile ", "").Replace(":", ""))};
+                    start = true;
+                }
+            }
+
+            Console.WriteLine(tiles.Count);
         }
     }
 }
