1ce0a6cacc5a0d6d89a3c49dfa0abcad7318a92f
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2021-02-19 19:31:23 +0000

    Update Program.cs

--- Day20/Program.cs
+++ Day20/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 
 namespace Day20
@@ -15,35 +14,65 @@ namespace Day20
             Content = new List<string>();
         }
 
+        
+
     }
+
+    public class Matrix
+    {
+        public char[][] Cells { get; set; }
+        public int Position { get; set; }
+        public Matrix()
+        {
+            Cells = new char[10][];
+            Position = 0;
+        }
+
+        public void Add(string line)
+        {
+            var s = new char[10];
+            for (int i = 0; i < 10; i++)
+            {
+                s[i] = line[i];
+            }
+            Cells[Position++] = s;
+        }
+
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var file = File.ReadAllLines("input.txt");
-            var tiles = new List<Tile>();
-            bool start = false;
-            Tile tile = null;
-            foreach (var row in file)
-            {
-                if (start && !string.IsNullOrEmpty(row))
-                {
-                    tile.Content.Add(row);
-                }
-                else if (string.IsNullOrEmpty(row))
-                {
-                    start = false;
-                    tiles.Add(tile);
-                }
-                if (row.Contains("Tile"))
-                {
-                    tile = new Tile {Id = int.Parse(row.Replace("Tile ", "").Replace(":", ""))};
-                    start = true;
-                }
-            }
+            string[][] a = new string[10][];
 
-            Console.WriteLine(tiles.Count);
-            string[][] a = new string[720][];
+
+
+            //var file = File.ReadAllLines("input.txt");
+            //var tiles = new List<Tile>();
+            //bool start = false;
+            //Tile tile = null;
+            //foreach (var row in file)
+            //{
+            //    if (start && !string.IsNullOrEmpty(row))
+            //    {
+            //        tile.Content.Add(row);
+            //    }
+            //    else if (string.IsNullOrEmpty(row))
+            //    {
+            //        start = false;
+            //        tiles.Add(tile);
+            //    }
+            //    if (row.Contains("Tile"))
+            //    {
+            //        tile = new Tile {Id = int.Parse(row.Replace("Tile ", "").Replace(":", ""))};
+            //        start = true;
+            //    }
+            //}
+
+            //Console.WriteLine(tiles.Count);
+            //string[][] a = new string[720][];
 
         }
     }
