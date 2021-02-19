5e638ffff62bdc018b787de3941ab683c0ddcdd0
JordiCorbilla <jordi.coll.corbilla@gmail.com>
2021-02-19 20:00:10 +0000

    Update Program.cs

--- Day20/Program.cs
+++ Day20/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 
 namespace Day20
@@ -38,6 +39,48 @@ namespace Day20
             Cells[Position++] = s;
         }
 
+        public void Print()
+        {
+            for(int i=0; i<10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write($"{Cells[i][j]}");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public void Print(char[][] s)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write($"{s[i][j]}");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public char[][] Rotate()
+        {
+            var s = new char[10][];
+            for (int i = 0; i < 10; i++)
+            {
+                s[i] = new char[10];
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    s[j][i] = Cells[i][j];
+                }
+            }
+
+            return s;
+        }
 
     }
 
@@ -47,29 +90,31 @@ namespace Day20
         {
             string[][] a = new string[10][];
 
-
-
-            //var file = File.ReadAllLines("input.txt");
-            //var tiles = new List<Tile>();
-            //bool start = false;
-            //Tile tile = null;
-            //foreach (var row in file)
-            //{
-            //    if (start && !string.IsNullOrEmpty(row))
-            //    {
-            //        tile.Content.Add(row);
-            //    }
-            //    else if (string.IsNullOrEmpty(row))
-            //    {
-            //        start = false;
-            //        tiles.Add(tile);
-            //    }
-            //    if (row.Contains("Tile"))
-            //    {
-            //        tile = new Tile {Id = int.Parse(row.Replace("Tile ", "").Replace(":", ""))};
-            //        start = true;
-            //    }
-            //}
+            var file = File.ReadAllLines("input.txt");
+            var tiles = new List<Matrix>();
+            bool start = false;
+            Matrix tile = null;
+            foreach (var row in file)
+            {
+                if (start && !string.IsNullOrEmpty(row))
+                {
+                    tile.Add(row);
+                }
+                else if (string.IsNullOrEmpty(row))
+                {
+                    start = false;
+                    tiles.Add(tile);
+                    tile.Print();
+                    Console.WriteLine();
+                    var s = tile.Rotate();
+                    tile.Print(s);
+                }
+                if (row.Contains("Tile"))
+                {
+                    tile = new Matrix();// { Id = int.Parse(row.Replace("Tile ", "").Replace(":", "")) };
+                    start = true;
+                }
+            }
 
             //Console.WriteLine(tiles.Count);
             //string[][] a = new string[720][];
